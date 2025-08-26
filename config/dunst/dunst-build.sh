d365b7605fd2d0dbfac9841bd7c0ebe297527067
deadonrip <deadonrip@gmail.com>
2025-08-26 00:13:23 -0400

    Made script to write to config file

--- config/dunst/dunst-build.sh
+++ config/dunst/dunst-build.sh
@@ -5,7 +5,6 @@ colour_file="$HOME/.cache/wal/colors.sh"
 if [[ -f $colour_file ]]
 then
     source $colour_file
-    pidof dunst && killall dunst
 
     txt=$color7
     bg=$color0
@@ -14,25 +13,112 @@ then
     normal=$color3
     urgent=$color2
 
-#        -lf/nf/cf color
-#            Defines the foreground color for low, normal and critical notifications respectively.
-# 
-#        -lb/nb/cb color
-#            Defines the background color for low, normal and critical notifications respectively.
-# 
-#        -lfr/nfr/cfr color
-#            Defines the frame color for low, normal and critical notifications respectively.
+cat <<EOF > $HOME/.config/dunst/dunstrc
+# See dunst(5) for all configuration options
+
+[global]
+    monitor = 0
+    follow = none
+    width = (100, 300)
+    height = (0, 300)
+    origin = top-right
+    offset = (5, 15)
+    scale = 0
+    notification_limit = 10
+
+    progress_bar = true
+    progress_bar_height = 14
+    progress_bar_frame_width = 0
+    progress_bar_min_width = 100
+    progress_bar_max_width = 300
+    progress_bar_corner_radius = 50
+	progress_bar_corners = bottom-left, top-right
+
+    icon_corner_radius = 0
+    icon_corners = all
+
+    indicate_hidden = yes
+    separator_height = 6
+    padding = 10
+    horizontal_padding = 8
+    text_icon_padding = 12
+    frame_width = 1
+    gap_size = 6
+    sort = yes
+    font = Pangolin 12
+    line_height = 0
+    markup = full
+    format = "<b>%s</b>\n%b"
+    alignment = left
+    vertical_alignment = center
+    show_age_threshold = -1
+    ellipsize = middle
+    ignore_newline = no
+    stack_duplicates = true
+    hide_duplicate_count = false
+    show_indicators = yes
+    enable_recursive_icon_lookup = true
+    icon_theme = Adwaita
+    icon_position = right
+    min_icon_size = 32
+    max_icon_size = 128
+    icon_path = /usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/
+    sticky_history = yes
+    history_length = 30
+    dmenu = /usr/bin/dmenu -l 10 -p dunst:
+    browser = /usr/bin/xdg-open
+    always_run_script = true
+    title = Dunst
+    class = Dunst
+    corner_radius = 10
+    corners = bottom, top-left
+    ignore_dbusclose = false
+
+    force_xwayland = false
+    force_xinerama = false
+
+    mouse_left_click = close_current
+    mouse_middle_click = do_action, close_current
+    mouse_right_click = close_all
+
+    transparency = 0
+    frame_color = "$normal"
+    separator_color = frame
+
+[experimental]
+    per_monitor_dpi = false
 
 
-    dunst -lf  "$txt" \
-        -lb  "$bg" \
-        -lfr "$low" \
-        -nf  "$txt" \
-        -nb  "$bg" \
-        -nfr "$normal" \
-        -cf  "$txt" \
-        -cb  "$bg" \
-        -cfr "$urgent" > /dev/null 2>&1 &
+[urgency_low]
+    background = "$bg"
+    foreground = "$fg"
+    frame_color = "$low"
+	highlight = "$low, $normal"
+    timeout = 20
+
+[urgency_normal]
+    background = "$bg"
+    foreground = "$fg"
+	frame_color = "$normal"
+	highlight = "$low, $normal"
+    timeout = 20
+    override_pause_level = 30
+    # Icon for notifications with normal urgency
+    default_icon = dialog-information
+
+[urgency_critical]
+    background = "$bg"
+    foreground = "$fg"
+    frame_color = "$urgent"
+	highlight = "$low, $normal"
+    timeout = 0
+    override_pause_level = 60
+    # Icon for notifications with critical urgency
+    default_icon = dialog-warning
+EOF
+
+pkill dunst && dunst &
+echo "restarted dunst"
 
     else
         echo "Oops, I think something went wrong. I can't find $color_file"
